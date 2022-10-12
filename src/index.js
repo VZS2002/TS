378208d6236fcd2ad818cc1188e2f3dac2f3660a
Varga Zsigmond <petrikdiak@gmail.com>
2022-10-12 15:03:13 +0200

    felhasznalo ellenorzes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-
-/*
 "use strict";
-let t = [1, 2, 3];
-t.filter((e => e > 2));
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('szoveg').addEventListener('input', (e) => {
-        let elem = e.currentTarget;
-        document.body.style.backgroundColor = elem.value;
+    var _a;
+    (_a = document.getElementById('reg')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', (e) => {
+        let felhasznalonev = document.getElementById('felhasznalonev').value;
+        if (felhasznalonev.length > 30 || felhasznalonev.length < 6) {
+            document.getElementById('hibaf').textContent = 'A felhasználónév 6 és 30 karakter közötti hosszúságú lehet';
+        }
+        else {
+            document.getElementById('hibaf').textContent = ' ';
+        }
+        if (felhasznalonev.includes('&') || felhasznalonev.includes('=') || felhasznalonev.includes('&') || felhasznalonev.includes('_') || felhasznalonev.includes('\'') || felhasznalonev.includes('-') || felhasznalonev.includes('+') || felhasznalonev.includes(',')) {
+            document.getElementById('hibaf').textContent = 'A felhasználónév nem engedélyezett karaktert tartalmaz.';
+        }
     });
 });
-*/
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-/*
-let t = [1,2,3];
-t.filter((e => e>2))
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('szoveg')!.addEventListener('input', (e) => {
-        let elem = e.currentTarget as HTMLInputElement;
-        document.body.style.backgroundColor = elem.value;
+
+    document.getElementById('reg')?.addEventListener('click', (e)=>{
+       
+      let felhasznalonev = (document.getElementById('felhasznalonev') as HTMLInputElement).value;
+
+
+    if (felhasznalonev.length > 30 ||  felhasznalonev.length <6) {
+    
+        (document.getElementById('hibaf') as HTMLInputElement).textContent = 'A felhasználónév 6 és 30 karakter közötti hosszúságú lehet';
+        
+    
+    } else  {
+        (document.getElementById('hibaf') as HTMLInputElement).textContent = ' '
+    }
+    
+    if (felhasznalonev.includes('&') ||felhasznalonev.includes('=') ||felhasznalonev.includes('&') ||felhasznalonev.includes('_') ||felhasznalonev.includes('\'') ||felhasznalonev.includes('-') ||felhasznalonev.includes('+') ||felhasznalonev.includes(',') ) {
+    
+        (document.getElementById('hibaf') as HTMLInputElement).textContent = 'A felhasználónév nem engedélyezett karaktert tartalmaz.';
+        
+    } 
+
+    })
+    
+    
     });
-});
-*/
