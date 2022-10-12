9e5f988842565569552666f4ae5f0f3190856d59
Varga Zsigmond <petrikdiak@gmail.com>
2022-10-12 12:42:51 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+/*
+"use strict";
+let t = [1, 2, 3];
+t.filter((e => e > 2));
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('szoveg').addEventListener('input', (e) => {
+        let elem = e.currentTarget;
+        document.body.style.backgroundColor = elem.value;
+    });
+});
+*/
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*
+let t = [1,2,3];
+t.filter((e => e>2))
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('szoveg')!.addEventListener('input', (e) => {
+        let elem = e.currentTarget as HTMLInputElement;
+        document.body.style.backgroundColor = elem.value;
+    });
+});
+*/
