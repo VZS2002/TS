897c0bb2a4159e31db995e5540220dd3c097cc2b
Varga Zsigmond <petrikdiak@gmail.com>
2022-10-13 12:17:21 +0200

    email

--- src/index.js
+++ src/index.js
@@ -9,8 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
         else {
             document.getElementById('hibaf').textContent = ' ';
         }
-        if (felhasznalonev.includes('&') || felhasznalonev.includes('=') || felhasznalonev.includes('&') || felhasznalonev.includes('_') || felhasznalonev.includes('\'') || felhasznalonev.includes('-') || felhasznalonev.includes('+') || felhasznalonev.includes(',')) {
+        if (felhasznalonev.includes('&') || felhasznalonev.includes('=') || felhasznalonev.includes('&') || felhasznalonev.includes('_') || felhasznalonev.includes('\'') || felhasznalonev.includes('-') || felhasznalonev.includes('+') || felhasznalonev.includes(',') || felhasznalonev.includes('<' || felhasznalonev.includes('>'))) {
             document.getElementById('hibaf').textContent = 'A felhasználónév nem engedélyezett karaktert tartalmaz.';
+        }
+        let email = document.getElementById('email').value;
+        const validateEmail = (email) => {
+            return String(email)
+                .toLowerCase()
+                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        };
+        if (validateEmail(email)) {
+            document.getElementById('hibae').textContent = ' ';
+        }
+        else {
+            document.getElementById('hibae').textContent = 'Hibás email';
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,29 @@ document.addEventListener('DOMContentLoaded', () => {
         (document.getElementById('hibaf') as HTMLInputElement).textContent = ' '
     }
     
-    if (felhasznalonev.includes('&') ||felhasznalonev.includes('=') ||felhasznalonev.includes('&') ||felhasznalonev.includes('_') ||felhasznalonev.includes('\'') ||felhasznalonev.includes('-') ||felhasznalonev.includes('+') ||felhasznalonev.includes(',') ) {
+    if (felhasznalonev.includes('&') ||felhasznalonev.includes('=') ||felhasznalonev.includes('&') ||felhasznalonev.includes('_') ||felhasznalonev.includes('\'') ||felhasznalonev.includes('-') ||felhasznalonev.includes('+') ||felhasznalonev.includes(',') ||felhasznalonev.includes('<' || felhasznalonev.includes('>'))  ) {
     
         (document.getElementById('hibaf') as HTMLInputElement).textContent = 'A felhasználónév nem engedélyezett karaktert tartalmaz.';
         
     } 
+    let email : string = (document.getElementById('email') as HTMLInputElement).value;
 
-    })
+    const validateEmail = (email : string) => {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
     
-    
+           if(validateEmail(email))
+           {
+            (document.getElementById('hibae') as HTMLElement).textContent = ' '
+           }
+
+           else{
+            (document.getElementById('hibae') as HTMLElement).textContent = 'Hibás email'
+           }
+
     });
+})
